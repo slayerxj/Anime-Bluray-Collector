5ff124409f6dfb51de0d6071d9986641c53110b9
slayerxj <slayerxj_reg@outlook.com>
2019-05-18 14:39:49 +0800

    Initial version.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AnimesComponent } from './animes/animes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimesComponent
   ],
   imports: [
     BrowserModule
