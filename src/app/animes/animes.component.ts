651656517ada20ebf7b79ccd0b675c79eef69d6b
slayerxj <slayerxj_reg@outlook.com>
2019-05-23 21:55:35 +0800

    Add toggle buttons to filter contents.

--- src/app/animes/animes.component.ts
+++ src/app/animes/animes.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Anime } from '../anime';
 import { ANIMES } from '../mock-animes';
 
 @Component({
@@ -8,9 +7,28 @@ import { ANIMES } from '../mock-animes';
   styleUrls: ['./animes.component.css']
 })
 export class AnimesComponent implements OnInit {
+  displayedColumns: string[] = ['time', 'name', 'weight', 'download'];
   animes = ANIMES;
+
+  isNew : boolean = false;
+  isComplete : boolean = false;
+  isRecent : boolean = false;
+
   constructor() { }
 
+  changed() {
+    // var currentTime = (new Date()).getTime();
+    // var deadline = currentTime - 1000 * 60 * 60 * 24 * 7;
+    if (!this.isNew && !this.isComplete) {
+      this.animes = ANIMES;
+      return;
+    }
+    var self = this;
+    this.animes = ANIMES.filter(function(anime) {
+      return ((anime.isNew === self.isNew) && (anime.isComplete === self.isComplete));
+    });
+    
+  }
   ngOnInit() {
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CdkTableModule} from '@angular/cdk/table';
+
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule,
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { AnimesComponent } from './animes/animes.component';
@@ -13,7 +55,13 @@ import { RankPipe } from './rank.pipe';
     RankPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CdkTableModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatTableModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
